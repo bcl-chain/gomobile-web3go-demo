fe77a1de9dd1810bab59ee537417e88d2892a804
kmyw <wangxiaodong@yuhu.tech>
2019-05-06 16:13:52 +0800

    first commit

---
+++ android/app/src/main/java/com/iosweb3godemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iosweb3godemo;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "iosweb3godemo";
+    }
+}
